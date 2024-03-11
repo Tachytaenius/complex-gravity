16ae6b101c135215d3c2ac27d7dd23d77d11f158
Tachytaenius <henryfthomson@gmail.com>
2024-03-11 17:54:22 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,125 @@
+local types = require("types")
+local complex = types.complex
+local vec2 = types.vec2
+
+local gravityStrength = complex(10, 0)
+local imaginaryColourScale = 150
+
+local particles
+
+function love.load()
+	particles = {}
+	for i = 1, 50 do
+		particles[i] = {
+			position = vec2(
+				complex(
+					love.math.random() * 200 - 50,
+					love.math.random() * 20 - 5
+				),
+				complex(
+					love.math.random() * 200 - 50,
+					love.math.random() * 20 - 5
+				)
+			),
+			velocity = vec2(
+				complex(
+					love.math.random() * 20 - 10,
+					love.math.random() * 2 - 1
+				),
+				complex(
+					love.math.random() * 20 - 10,
+					love.math.random() * 2 - 1
+				)
+			),
+			mass = complex(love.math.random() * 8 + 2, love.math.random() * 2 + 1)
+		}
+		particles[i].radius = complex.abs(particles[i].mass).re / 2
+	end
+end
+
+function love.update(dt)
+	for i = 1, #particles - 1 do
+		local particleA = particles[i]
+		for j = i + 1, #particles do
+			local particleB = particles[j]
+
+			local difference = particleB.position - particleA.position
+			local distance = #difference
+			local direction = difference / distance
+
+			-- local distanceFactor = math.min(1, 1 / (distance * distance).re)
+			local distanceFactor = math.min(1, 1 / distance.re)
+			local force = gravityStrength * particleA.mass * particleB.mass * distanceFactor
+
+			particleA.velocity = particleA.velocity + direction * force * dt / particleA.mass
+			particleB.velocity = particleB.velocity - direction * force * dt / particleB.mass
+		end
+	end
+	for i = 1, #particles do
+		local particle = particles[i]
+		particle.position = particle.position + particle.velocity * dt
+	end
+end
+
+function love.draw()
+	local w, h = love.graphics.getDimensions()
+
+	love.graphics.translate(w / 2, h / 2)
+
+	for i = 1, #particles do
+		local particle = particles[i]
+
+		-- Different approaches to drawing
+
+		love.graphics.setColor(0.5, particle.position.x.im / imaginaryColourScale + 0.5, particle.position.y.im / imaginaryColourScale + 0.5)
+		love.graphics.circle("fill", particle.position.x.re, particle.position.y.re, particle.radius)
+
+		-- love.graphics.setColor(0.5, particle.position.y.re / imaginaryColourScale + 0.5, particle.position.y.im / imaginaryColourScale + 0.5)
+		-- love.graphics.circle("fill", particle.position.x.re, particle.position.x.im, particle.radius)
+
+		-- love.graphics.setColor(0.5, particle.position.y.re / imaginaryColourScale + 0.5, particle.position.x.im / imaginaryColourScale + 0.5)
+		-- love.graphics.circle("fill", particle.position.x.re, particle.position.y.im, particle.radius)
+	end
+	love.graphics.setColor(1, 1, 1)
+end
+
+-- sqrt demonstration
+-- function love.draw()
+-- 	local w, h = love.graphics.getDimensions()
+-- 	local scale = 75
+
+-- 	love.graphics.setBlendMode("add")
+
+-- 	love.graphics.translate(w / 2, h / 2)
+-- 	love.graphics.scale(1, -1)
+
+-- 	love.graphics.scale(scale)
+-- 	love.graphics.setLineWidth(1 / scale)
+
+-- 	local mx, my = love.mouse.getPosition()
+-- 	local x, y = (mx - w / 2) / scale, -(my - h / 2) / scale
+-- 	if love.keyboard.isDown("lshift") then
+-- 		y = 0
+-- 	end
+-- 	local a = complex(x, y)
+-- 	local b = complex.sqrt(a)
+-- 	local c = b * b
+
+-- 	love.graphics.setPointSize(8)
+-- 	love.graphics.setColor(1, 0, 0)
+-- 	love.graphics.points(a.re, a.im)
+-- 	love.graphics.setColor(0, 1, 0)
+-- 	love.graphics.points(b.re, b.im)
+-- 	love.graphics.setColor(0, 0, 1)
+-- 	love.graphics.points(c.re, c.im)
+
+-- 	love.graphics.setPointSize(5)
+-- 	love.graphics.setColor(0.5, 0.5, 0.5)
+-- 	love.graphics.points(1, 0) -- 1
+-- 	love.graphics.points(0, 1) -- i
+-- 	love.graphics.points(-1, 0) -- -1
+-- 	love.graphics.points(0, -1) -- -i
+-- 	love.graphics.circle("line", 0, 0, 1)
+-- 	love.graphics.line(0, -h / 2, 0, h / 2)
+-- 	love.graphics.line(-w / 2, 0, w / 2, 0)
+-- end
---
+++ types.lua
@@ -0,0 +1,334 @@
+-- NOTE: Not perfect, just works
+
+local ffi = require("ffi")
+
+local ffi_istype = ffi.istype
+
+-- Complex numbers
+
+local newComplexRaw = ffi.typeof("complex")
+local function newComplex(re, im)
+	re = re or 0
+	im = im or 0
+	return newComplexRaw(re, im)
+end
+
+local function clone(z)
+	return newComplexRaw(z.re, z.im)
+end
+
+local function parts(z)
+	return z.re, z.im
+end
+
+local i = newComplexRaw(0, 1)
+
+local function complexAbs(z)
+	return newComplexRaw(math.sqrt(z.re ^ 2 + z.im ^ 2), 0)
+end
+
+local function complexAbs2(z)
+	return newComplexRaw(z.re ^ 2 + z.im ^ 2, 0)
+end
+
+local function complexArg(z)
+	return newComplexRaw(math.atan2(z.im, z.re), 0)
+end
+
+local function realSqrtNegative(x)
+	if x < 0 then
+		return newComplexRaw(0, math.sqrt(-x))
+	end
+	return newComplexRaw(math.sqrt(x), 0)
+end
+
+local function complexSqrt(z)
+	if z.im == 0 then
+		return realSqrtNegative(z.re)
+	end
+	local absZ = complexAbs(z)
+	return
+		realSqrtNegative((absZ + z.re).re / 2) +
+		i * z.im / math.abs(z.im) * realSqrtNegative((absZ - z.re).re / 2)
+end
+
+local function complexConjugate(z)
+	return newComplexRaw(z.re, -z.im)
+end
+
+ffi.metatype("complex", {
+	__add = function(a, b)
+		if type(a) == "number" then
+			return newComplexRaw(a + b.re, b.im)
+		elseif type(b) == "number" then
+			return newComplexRaw(a.re + b, a.im)
+		else
+			return newComplexRaw(a.re + b.re, a.im + b.im)
+		end
+	end,
+	__sub = function(a, b)
+		if type(a) == "number" then
+			return newComplexRaw(a - b.re, -b.im)
+		elseif type(b) == "number" then
+			return newComplexRaw(a.re - b, a.im)
+		else
+			return newComplexRaw(a.re - b.im, a.re - b.im)
+		end
+	end,
+	__unm = function(z)
+		return newComplexRaw(-z.re, -z.im)
+	end,
+	__mul = function(a, b)
+		if type(a) == "number" then
+			return newComplexRaw(a * b.re, a * b.im)
+		elseif type(b) == "number" then
+			return newComplexRaw(a.re * b, a.im * b)
+		else
+			return newComplexRaw(
+				a.re * b.re - a.im * b.im,
+				a.im * b.re + a.re * b.im
+			)
+		end
+	end,
+	__div = function(a, b)
+		if type(a) == "number" then
+			newComplexRaw(
+				a * b.re / (b.re ^ 2 + b.im ^ 2),
+				-a * b.im / (b.re ^ 2 + b.im ^ 2)
+			)
+		elseif type(b) == "number" then
+			return newComplexRaw(
+				a.re / b,
+				a.im / b
+			)
+		else
+			return newComplexRaw(
+				(a.re * b.re + a.im * b.im) / (b.re ^ 2 + b.im ^ 2),
+				(a.im * b.re - a.re * b.im) / (b.re ^ 2 + b.im ^ 2)
+			)
+		end
+	end,
+	__len = complexAbs,
+	__tostring = function(z)
+		return string.format("%f + $fi", z.re, z.im)
+	end
+})
+
+local complex = setmetatable({
+	new = newComplex,
+	abs = complexAbs,
+	abs2 = complexAbs2,
+	arg = complexArg,
+	sqrt = complexSqrt,
+	parts = parts,
+	clone = clone,
+	conjugate = complexConjugate,
+	i = i
+}, {
+	__call = function(_, re, im)
+		return newComplex(re, im)
+	end
+})
+
+-- Vectors
+-- The B functions were ones based on my original understanding of complex vector length and complex vector dot
+
+ffi.cdef([=[
+	typedef struct {
+		complex x, y;
+	} vec2;
+]=])
+
+local rawnew = ffi.typeof("vec2")
+local function new(x, y)
+	x = x or 0
+	y = y or x
+	return rawnew(x, y)
+end
+
+local sqrt, sin, cos = complexSqrt, math.sin, math.cos
+
+local function length(v)
+	return sqrt(complexAbs2(v.x) + complexAbs2(v.y))
+end
+
+local function length2(v)
+	return newComplexRaw(complexAbs2(v.x) + complexAbs2(v.y))
+end
+
+local function distance(a, b)
+	return sqrt(complexAbs2(b.x - a.x) + complexAbs2(b.y - a.y))
+end
+
+local function distance2(a, b)
+	return complexAbs2(b.x - a.x) + complexAbs2(b.y - a.y)
+end
+
+local function lengthB(v)
+	local x, y = v.x, v.y
+	return sqrt(x * x + y * y)
+end
+
+local function lengthB2(v)
+	local x, y = v.x, v.y
+	return x * x + y * y
+end
+
+local function distanceB(a, b)
+	local x, y = b.x - a.x, b.y - a.y
+	return sqrt(x * x + y * y)
+end
+
+local function distanceB2(a, b)
+	local x, y = b.x - a.x, b.y - a.y
+	return x * x + y * y
+end
+
+local function dot(a, b)
+	return a.x * complexConjugate(a.y) + a.y * complexConjugate(b.y)
+end
+
+local function dotB(a, b)
+	return a.x * b.x + a.y * b.y
+end
+
+local function normalise(v)
+	return v / length(v)
+end
+
+local function normaliseB(v)
+	return v / lengthB(v)
+end
+
+local function reflect(incident, normal)
+	return incident - 2 * dot(normal, incident) * normal
+end
+
+-- k < 0 but k is complex?
+-- local function refract(incident, normal, eta)
+-- 	local ndi = dot(normal, incident)
+-- 	local k = 1 - eta * eta * (1 - ndi * ndi)
+-- 	if k < 0 then
+-- 		return rawnew(0, 0)
+-- 	else
+-- 		return eta * incident - (eta * ndi + sqrt(k)) * normal
+-- 	end
+-- end
+
+local function rotate(v, a)
+	local x, y = v.x, v.y
+	return rawnew(
+		x * cos(a) - y * sin(a), 0,
+		y * cos(a) + x * sin(a), 0
+	)
+end
+
+local function fromAngle(a)
+	return rawnew(cos(a), 0, sin(a), 0)
+end
+
+local function components(v)
+	return v.x, v.y
+end
+
+local function componentsParts(v)
+	return v.x.re, v.x.im, v.y.re, v.y.im
+end
+
+local function clone(v)
+	return rawnew(v.x, v.y)
+end
+
+local function isComplex(object)
+	return type(object) == "cdata" and ffi_istype("complex", object)
+end
+
+ffi.metatype("vec2", {
+	__add = function(a, b)
+		if type(a) == "number" or isComplex(a) then
+			return rawnew(a + b.x, a + b.y)
+		elseif type(b) == "number" or isComplex(b) then
+			return rawnew(a.x + b, a.y + b)
+		else
+			return rawnew(a.x + b.x, a.y + b.y)
+		end
+	end,
+	__sub = function(a, b)
+		if type(a) == "number" or isComplex(a) then
+			return rawnew(a - b.x, a - b.y)
+		elseif type(b) == "number" or isComplex(b) then
+			return rawnew(a.x - b, a.y - b)
+		else
+			return rawnew(a.x - b.x, a.y - b.y)
+		end
+	end,
+	__unm = function(v)
+		return rawnew(-v.x, -v.y)
+	end,
+	__mul = function(a, b)
+		if type(a) == "number" or isComplex(a) then
+			return rawnew(a * b.x, a * b.y)
+		elseif type(b) == "number" or isComplex(b) then
+			return rawnew(a.x * b, a.y * b)
+		else
+			return rawnew(a.x * b.x, a.y * b.y)
+		end
+	end,
+	__div = function(a, b)
+		if type(a) == "number" or isComplex(a) then
+			return rawnew(a / b.x, a / b.y)
+		elseif type(b) == "number" or isComplex(b) then
+			return rawnew(a.x / b, a.y / b)
+		else
+			return rawnew(a.x / b.x, a.y / b.y)
+		end
+	end,
+	__mod = function(a, b)
+		if type(a) == "number" or isComplex(a) then
+			return rawnew(a % b.x, a % b.y)
+		elseif type(b) == "number" or isComplex(b) then
+			return rawnew(a.x % b, a.y % b)
+		else
+			return rawnew(a.x % b.x, a.y % b.y)
+		end
+	end,
+	__eq = function(a, b)
+		local isVec2 = type(b) == "cdata" and ffi_istype("vec2", b)
+		return isVec2 and a.x == b.x and a.y == b.y
+	end,
+	__len = length,
+	__tostring = function(v)
+		return "vec2(" .. tostring(v.x) ..  ", " .. tostring(v.y) .. ")"
+	end
+})
+
+local vec2 = setmetatable({
+	new = new,
+	length = length,
+	length2 = length2,
+	distance = distance,
+	distance2 = distance2,
+	lengthB = lengthB,
+	lengthB2 = lengthB2,
+	distanceB = distanceB,
+	distanceB2 = distanceB2,
+	dot = dot,
+	dotB = dotB,
+	normalise = normalise,
+	normaliseB = normaliseB,
+	normalize = normalise,
+	normalizeB = normaliseB,
+	reflect = reflect,
+	-- refract = refract,
+	rotate = rotate,
+	fromAngle = fromAngle,
+	components = components,
+	componentsParts = componentsParts,
+	clone = clone
+}, {
+	__call = function(_, x, y)
+		return new(x, y)
+	end
+})
+
+return {complex = complex, vec2 = vec2}
